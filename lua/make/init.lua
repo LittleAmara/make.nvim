e6e7286d4e1505b7bd2a9b84e01d0ad926e7f36f
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-08-28 18:51:58 +0200

    feat(lua: make): add run function

--- lua/make/init.lua
+++ lua/make/init.lua
@@ -52,4 +52,54 @@ M.compile_command = function()
   vim.keymap.set("n", "q", "<cmd>close!<cr>", { silent = true, buffer = bufnr })
 end
 
+--- Sends a message to the term buffer used to display stdout and stderr.
+---@param channel_id number
+---@param data string[]
+---@param is_stderr boolean
+local function channel_callback(channel_id, data, is_stderr)
+  for _, raw_line in ipairs(data) do
+    local line = raw_line ~= "" and raw_line .. "\r\n" or raw_line
+    vim.api.nvim_chan_send(channel_id, line)
+    if is_stderr then
+      vim.fn.setqflist({}, "a", {
+        lines = { raw_line },
+        title = "Compile errors",
+      })
+    end
+  end
+end
+
+M.run = function()
+  -- TODO: manage only one buffer instead of creating one everytime
+  local bufnr = vim.api.nvim_create_buf(true, true)
+  assert(bufnr, "failed to create buffer")
+
+  local chan_id = vim.api.nvim_open_term(bufnr, {})
+
+  -- This is currently (nvim-0.9.1) and we cannot create vertical windows via the nvim api, see
+  -- https://github.com/neovim/neovim/issues/14315
+  -- see `:h vertical`, `:h botright` and `:h sbuffer` if you do not understand this line.
+  vim.cmd("vertical botright sbuffer" .. tostring(bufnr))
+
+  -- Setup the look of the window
+  local winnr = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_option(winnr, "number", false)
+  vim.api.nvim_win_set_option(winnr, "relativenumber", false)
+
+  -- Clear the qf list
+  vim.fn.setqflist({}, "r")
+
+  vim.fn.jobstart(vim.opt.makeprg:get(), {
+    on_exit = function(_, exit_code)
+      vim.api.nvim_chan_send(chan_id, ("[Process exited %d]"):format(exit_code))
+    end,
+    on_stdout = function(_, data)
+      channel_callback(chan_id, data, false)
+    end,
+    on_stderr = function(_, data)
+      channel_callback(chan_id, data, true)
+    end,
+  })
+end
+
 return M
